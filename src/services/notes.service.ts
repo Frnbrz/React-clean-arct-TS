f7c2081432c36e0701f3df3f1d2bde0d14f7d0c2
Frnbrz <frnbrz@gmail.com>
2023-01-07 01:17:38 +0100

    axios interceptor and code error matcher

--- src/services/notes.service.ts
+++ src/services/notes.service.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/api/'
-const noteUrl = baseUrl + 'notes/'
+const noteUrl = baseUrl + 'notes/a'
 
 export const getNotes = () => {
   return axios.get(noteUrl)
 }
 
 export const getNote = (noteId: number) => {
-  return fetch(noteUrl + noteId).then((res) => res.json())
+  return axios.get(noteUrl + noteId)
 }
--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,2 +1,4 @@
 export { default as RoutesWithNotFound } from './RoutesWithNotFound.utility'
+export * from './getValidationsError'
+export * from './index'
 export * from './localStorage.utility'

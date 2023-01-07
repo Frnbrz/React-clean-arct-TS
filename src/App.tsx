93b5e37f467f322712b2a8c0e02fb47e7047507d
Frnbrz <frnbrz@gmail.com>
2023-01-07 11:09:19 +0100

    general controlled errors

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { AuthGuard, RoleGuard } from '@/guards'
 import { PrivateRoutes, PublicRoutes, Roles } from '@/models'
 import { Dashboard } from '@/pages/Private'
 import store from '@/redux/store'
-import { RoutesWithNotFound } from '@/utilities'
+import { ErrorBoundary, RoutesWithNotFound } from '@/utilities'
 import { SnackbarProvider } from 'notistack'
 import { lazy, Suspense } from 'react'
 import { Provider } from 'react-redux'
@@ -18,29 +18,29 @@ function App() {
   return (
     <div className='App'>
       <SnackbarProvider>
-
         <SnackbarUtilitiesConfigurator />
-
         <Suspense fallback={<>Cargando</>}>
           <Provider store={store}>
             <BrowserRouter>
               <ButtonAppBar />
-              <RoutesWithNotFound>
-                <Route
-                  path='/'
-                  element={<Navigate to={PrivateRoutes.PRIVATE} />}
-                />
-                <Route path={PublicRoutes.LOGIN} element={<Login />} />
-                <Route element={<AuthGuard privateValidation />}>
+              <ErrorBoundary fallbackComponent={<h2>Hubo un error</h2>}>
+                <RoutesWithNotFound>
                   <Route
-                    path={`${PrivateRoutes.PRIVATE}/*`}
-                    element={<Private />}
+                    path='/'
+                    element={<Navigate to={PrivateRoutes.PRIVATE} />}
                   />
-                </Route>
-                <Route element={<RoleGuard rol={Roles.ADMIN} />}>
-                  <Route path={PrivateRoutes.DASHBOARD} element={<Dashboard />} />
-                </Route>
-              </RoutesWithNotFound>
+                  <Route path={PublicRoutes.LOGIN} element={<Login />} />
+                  <Route element={<AuthGuard privateValidation />}>
+                    <Route
+                      path={`${PrivateRoutes.PRIVATE}/*`}
+                      element={<Private />}
+                    />
+                  </Route>
+                  <Route element={<RoleGuard rol={Roles.ADMIN} />}>
+                    <Route path={PrivateRoutes.DASHBOARD} element={<Dashboard />} />
+                  </Route>
+                </RoutesWithNotFound>
+              </ErrorBoundary>
             </BrowserRouter>
           </Provider>
         </Suspense>

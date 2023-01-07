75d7bbccc9b595420ba386daec03d9432d241a60
Frnbrz <frnbrz@gmail.com>
2023-01-07 01:16:45 +0100

    added toast provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import { PrivateRoutes, PublicRoutes, Roles } from '@/models'
 import { Dashboard } from '@/pages/Private'
 import store from '@/redux/store'
 import { RoutesWithNotFound } from '@/utilities'
+import { SnackbarProvider } from 'notistack'
 import { lazy, Suspense } from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Navigate, Route } from 'react-router-dom'
+import { SnackbarUtilitiesConfigurator } from './utilities/snackbarManager'
 
 const Login = lazy(() => import('./pages/Login/Login'))
 const Private = lazy(() => import('./pages/Private/Private'))
@@ -15,29 +17,34 @@ const Private = lazy(() => import('./pages/Private/Private'))
 function App() {
   return (
     <div className='App'>
-      <Suspense fallback={<>Cargando</>}>
-        <Provider store={store}>
-          <BrowserRouter>
-            <ButtonAppBar />
-            <RoutesWithNotFound>
-              <Route
-                path='/'
-                element={<Navigate to={PrivateRoutes.PRIVATE} />}
-              />
-              <Route path={PublicRoutes.LOGIN} element={<Login />} />
-              <Route element={<AuthGuard privateValidation />}>
+      <SnackbarProvider>
+
+        <SnackbarUtilitiesConfigurator />
+
+        <Suspense fallback={<>Cargando</>}>
+          <Provider store={store}>
+            <BrowserRouter>
+              <ButtonAppBar />
+              <RoutesWithNotFound>
                 <Route
-                  path={`${PrivateRoutes.PRIVATE}/*`}
-                  element={<Private />}
+                  path='/'
+                  element={<Navigate to={PrivateRoutes.PRIVATE} />}
                 />
-              </Route>
-              <Route element={<RoleGuard rol={Roles.ADMIN} />}>
-                <Route path={PrivateRoutes.DASHBOARD} element={<Dashboard />} />
-              </Route>
-            </RoutesWithNotFound>
-          </BrowserRouter>
-        </Provider>
-      </Suspense>
+                <Route path={PublicRoutes.LOGIN} element={<Login />} />
+                <Route element={<AuthGuard privateValidation />}>
+                  <Route
+                    path={`${PrivateRoutes.PRIVATE}/*`}
+                    element={<Private />}
+                  />
+                </Route>
+                <Route element={<RoleGuard rol={Roles.ADMIN} />}>
+                  <Route path={PrivateRoutes.DASHBOARD} element={<Dashboard />} />
+                </Route>
+              </RoutesWithNotFound>
+            </BrowserRouter>
+          </Provider>
+        </Suspense>
+      </SnackbarProvider>
     </div>
   )
 }
